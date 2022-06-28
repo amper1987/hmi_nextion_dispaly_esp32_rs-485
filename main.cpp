df69802f5109271d5ccc1b79f428c050c4c4c046
amper1987 <i.zavgorodni@aetiinc.comgit config --global user.email i.zavgorodni@aetiinc.com>
2022-06-28 15:04:08 -0600

    Status and RSSI are added need to be tested                                     all variables has been renamed all tarts with o now

--- main.cpp
+++ main.cpp
@@ -14,54 +14,64 @@
 #define RXD1 9
 #define TXD1 10
 #define DE_RE 18
-#define CHARGER_SIZE 256 
+#define CHARGER_SIZE 256
+#define DATA_SIZE 32 
 
+uint16_t i;
+uint32_t status_register = 0;
+uint8_t status_value;
 uint32_t ten_second_timeout_flag = 0;
 hw_timer_t * timer = NULL;
 uint16_t length = 0x0001;
 uint8_t xmit_byte; 
-char array[CHARGER_SIZE];
-char buffer1[10];
-char buffer2[10];
-char buffer3[10];
-char buffer4[10];
-char buffer5[10];
-char buffer6[10];
-char buffer7[10];
-char buffer8[10];
-char buffer9[10];
-char buffer10[10];
-char buffer11[10];
-char buffer12[10];
-char buffer13[10];
-char *ptr = array;
-int number1 = 7;
-int number2 = 11;
-int number3 = 15;
-int number4 = 19;
-int number5 = 23;
-int number6 = 27;
-int number7 = 31;
-int number8 = 35;
-int number9 = 39;
-int number10 = 43;
-int number11 = 47;
-int number12 = 51;
-int number13 = 55;
-float float_number1;
-float float_number2;
-float float_number3;
-float float_number4;
-float float_number5;
-float float_number6;
-float float_number7;
-float float_number8;
-float float_number9;
-float float_number10;
-float float_number11;
-float float_number12;
-float float_number13;
-int i;
+char measurement_buff[CHARGER_SIZE];
+char rssi_buff[DATA_SIZE];
+char status_buff[DATA_SIZE];
+char power_board_output_current_avg_buff[DATA_SIZE];
+char power_board_output_current_max_buff[DATA_SIZE];
+char power_board_output_current_min_buff[DATA_SIZE];
+char power_board_output_voltage_buff[DATA_SIZE];
+char output_current_avg_buff[DATA_SIZE];
+char output_current_max_buff[DATA_SIZE];
+char output_current_min_buff[DATA_SIZE];
+char output_voltage_buff[DATA_SIZE];
+char generator_battery_voltage_buff[DATA_SIZE];
+char tempearure_sensor_0_buff[DATA_SIZE];
+char temperature_sensor_1_buff[DATA_SIZE];
+char temperature_sensor_2_buff[DATA_SIZE];
+char temperature_sensor_3_buff[DATA_SIZE];
+char *ptr = measurement_buff;
+uint8_t rssi_index = 3;
+uint8_t status_index = 7;
+uint8_t power_board_output_current_avg_index =11;
+uint8_t power_board_output_current_max_index = 15;
+uint8_t power_board_output_current_min_index = 19;
+uint8_t power_board_output_voltage_index = 23;
+uint8_t output_current_avg_index = 27;
+uint8_t output_current_max_index = 31;
+uint8_t output_current_min_index = 35;
+uint8_t output_voltage_index = 39;
+uint8_t generator_battery_voltage_index = 43;
+uint8_t tempearature_sensor_0_index = 47;
+uint8_t temperature_sensor_1_index = 51;
+uint8_t temperature_sensor_2_index = 55;
+uint8_t temperature_sensor_3_index = 59;
+float rssi;
+float status;
+float power_board_output_current_avg;
+float power_board_output_current_max;
+float power_board_output_current_min;
+float power_board_output_voltage;
+float output_current_avg;
+float output_current_max;
+float output_current_min;
+float output_voltage;
+float generator_battery_voltage;
+float tempearature_sensor_0;
+float temperature_sensor_1;
+float temperature_sensor_2;
+float temperature_sensor_3;
+
 
 void IRAM_ATTR onTimer()
 {
@@ -69,13 +79,25 @@ void IRAM_ATTR onTimer()
 }
 
 //prototypes declaration
-void parse_data (int index1, int index2, int index3, int index4,
-                 int index5, int index6, int index7, int index8,
-                 int index9, int index10, int index11, int index12,
-                 int index13);
-float bytes_to_float ( char *s);
+void parse_data (uint8_t rssi_index,
+                 uint8_t status_index,
+                 uint8_t power_board_output_current_avg_index,
+                 uint8_t power_board_output_current_max_index,
+                 uint8_t power_board_output_current_min_index,
+                 uint8_t power_board_output_voltage_index,
+                 uint8_t output_current_avg_index,
+                 uint8_t output_current_max_index, 
+                 uint8_t output_current_min_index,
+                 uint8_t output_voltage_index,
+                 uint8_t generator_battery_voltage_index,
+                 uint8_t tempearature_sensor_0_index,
+                 uint8_t temperature_sensor_1_index,
+                 uint8_t temperature_sensor_2_index,
+                 uint8_t temperature_sensor_3_index);
+float bytes_to_float (char *s);
 void send_command (void);
 void hmi_display_data (void);
+void receive_data (void);
  
 void setup() 
 {
@@ -86,7 +108,7 @@ void setup()
   nexInit();
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 10000000, true);
+  timerAlarmWrite(timer, 5000000, true);
   timerAlarmEnable(timer);
 }
 
@@ -95,63 +117,84 @@ void loop()
   if(ten_second_timeout_flag)
   {
   digitalWrite(DE_RE, HIGH); // set DE and RE to active HIGH TX mode
-  send_command(); 
+  send_command();
+  digitalWrite(DE_RE, LOW); // set DE and RE to active LOW RX mode 
   delay(5);
-  parse_data(number1, number2, number3, number4,
-             number5, number6, number7, number8,
-             number9, number10, number11, number12,
-             number13);
-  Serial.write(buffer1, 4);
+  receive_data();
+  parse_data(rssi_index,
+             status_index,
+             power_board_output_current_avg_index,
+             power_board_output_current_max_index,
+             power_board_output_current_min_index,
+             power_board_output_voltage_index,
+             output_current_avg_index,
+             output_current_max_index, 
+             output_current_min_index,
+             output_voltage_index,
+             generator_battery_voltage_index,
+             tempearature_sensor_0_index,
+             temperature_sensor_1_index,
+             temperature_sensor_2_index,
+             temperature_sensor_3_index);
   hmi_display_data();
-  memset(buffer1,0,sizeof(buffer1));
-  memset(array,0,sizeof(array));
+  memset(measurement_buff,0,sizeof(measurement_buff));
   ten_second_timeout_flag = 0;
   }
 } 
-//parse data based on the indexes num in the function
-void parse_data (int index1, int index2, int index3, int index4,
-                 int index5, int index6, int index7, int index8,
-                 int index9, int index10, int index11, int index12,
-                 int index13)
+//receive data and store in measurement_buff
+void receive_data (void)
 {
   if(Serial2.available())
   {
-    for (i = 0; i<sizeof(array); i++)
+    for (i = 0; i<sizeof(measurement_buff); i++)
     {
-    array[i] = Serial2.read();
+    measurement_buff[i] = Serial2.read();
     }
-      if ((array[0] == 0xFE) && (array[2] == 0x38))
-     { 
-      strlen (ptr = ptr+=index1);//advance pointer by index1 value in array
-      memcpy (buffer1, ptr, 4);//copy 4 bytes of array to buffer1
-      strlen (ptr = ptr-=index1);//subtract pointer by index1 value in array
-      strlen (ptr = ptr+=index4);//advance pointer by index4 value in array
-      memcpy (buffer4, ptr, 4);//copy 4 bytes of array to buffer4
-      strlen (ptr = ptr-=index4);//subtract pointer by index4 value in array
-      strlen (ptr = ptr+=index5);//advance pointer by index5 value in array
-      memcpy (buffer5, ptr, 4);//copy 4 bytes of array to buffer5
-      strlen (ptr = ptr-=index5);//subtract pointer by index5 value in array
-      strlen (ptr = ptr+=index8);//advance pointer by index8 value in array
-      memcpy (buffer8, ptr, 4);//copy 4 bytes of array to buffer8
-      strlen (ptr = ptr-=index8);//subtract pointer by index8 value in array
-      strlen (ptr = ptr+=index9);//advance pointer by index9 value in array
-      memcpy (buffer9, ptr, 4);//copy 4 bytes of array to buffer9
-      strlen (ptr = ptr-=index9);//subtract pointer by index9 value in array
-      strlen (ptr = ptr+=index10);//advance pointer by index10 value in array
-      memcpy (buffer10, ptr, 4);//copy 4 bytes of array to buffer10
-      strlen (ptr = ptr-=index10);//subtract pointer by index10 value in array
-      strlen (ptr = ptr+=index11);//advance pointer by index11 value in array
-      memcpy (buffer11, ptr, 4);//copy 4 bytes of array to buffer11
-      strlen (ptr = ptr-=index11);//subtract pointer by index11 value in array
-      strlen (ptr = ptr+=index12);//advance pointer by index12 value in array
-      memcpy (buffer12, ptr, 4);//copy 4 bytes of array to buffer12
-      strlen (ptr = ptr-=index12);//subtract pointer by index12 value in array
-      strlen (ptr = ptr+=index13);//advance pointer by index13 value in array
-      memcpy (buffer13, ptr, 4);//copy 4 bytes of array to buffer13
-      strlen (ptr = ptr-=index13);//subtract pointer by index13 value in array
-    }
-  }     
+  }
 }
+//parse data based on the indexes num in the function
+void parse_data (uint8_t index0, uint8_t index1, uint8_t index2, uint8_t index3,
+                 uint8_t index4, uint8_t index5, uint8_t index6, uint8_t index7,
+                 uint8_t index8, uint8_t index9, uint8_t index10, uint8_t index11,
+                 uint8_t index12, uint8_t index13, uint8_t index14)
+{
+  if ((measurement_buff[0] == 0xFE) && (measurement_buff[2] == 0x3C))
+  {
+  strlen (ptr = ptr+=index0);//advance pointer by index1 value in array
+  memcpy (rssi_buff, ptr, 4);//copy 4 bytes of array to rssi_buff
+  strlen (ptr = ptr-=index0);//subtract pointer by index1 value in array 
+  strlen (ptr = ptr+=index1);//advance pointer by index1 value in array
+  memcpy (status_buff, ptr, 4);//copy 4 bytes of array to status_buff
+  strlen (ptr = ptr-=index1);//subtract pointer by index1 value in array 
+  strlen (ptr = ptr+=index2);//advance pointer by index1 value in array
+  memcpy (power_board_output_current_avg_buff, ptr, 4);//copy 4 bytes of array power_board_output_current_avg_buff
+  strlen (ptr = ptr-=index2);//subtract pointer by index1 value in array
+  strlen (ptr = ptr+=index5);//advance pointer by index4 value in array
+  memcpy (power_board_output_voltage_buff, ptr, 4);//copy 4 bytes of array to power_board_output_voltage_buff
+  strlen (ptr = ptr-=index5);//subtract pointer by index4 value in array
+  strlen (ptr = ptr+=index6);//advance pointer by index5 value in array
+  memcpy (output_current_avg_buff, ptr, 4);//copy 4 bytes of array to output_current_avg_buff
+  strlen (ptr = ptr-=index6);//subtract pointer by index5 value in array
+  strlen (ptr = ptr+=index9);//advance pointer by index8 value in array
+  memcpy (output_voltage_buff, ptr, 4);//copy 4 bytes of array to output_voltage_buff
+  strlen (ptr = ptr-=index9);//subtract pointer by index8 value in array
+  strlen (ptr = ptr+=index10);//advance pointer by index9 value in array
+  memcpy (generator_battery_voltage_buff, ptr, 4);//copy 4 bytes of array to generator_battery_voltage_buff
+  strlen (ptr = ptr-=index10);//subtract pointer by index9 value in array
+  strlen (ptr = ptr+=index11);//advance pointer by index10 value in array
+  memcpy (tempearure_sensor_0_buff, ptr, 4);//copy 4 bytes of array to tempearure_sensor_0_buff
+  strlen (ptr = ptr-=index11);//subtract pointer by index10 value in array
+  strlen (ptr = ptr+=index12);//advance pointer by index11 value in array
+  memcpy (temperature_sensor_1_buff, ptr, 4);//copy 4 bytes of array to tempearure_sensor_1_buff
+  strlen (ptr = ptr-=index12);//subtract pointer by index11 value in array
+  strlen (ptr = ptr+=index13);//advance pointer by index12 value in array
+  memcpy (temperature_sensor_2_buff, ptr, 4);//copy 4 bytes of array to temperature_sensor_2_buff
+  strlen (ptr = ptr-=index13);//subtract pointer by index12 value in array
+  strlen (ptr = ptr+=index14);//advance pointer by index13 value in array
+  memcpy (temperature_sensor_3_buff, ptr, 4);//copy 4 bytes of array to temperature_sensor_3_buff
+  strlen (ptr = ptr-=index14);//subtract pointer by index13 value in array
+  }
+} 
 
 /*This function saves the buffer contain inside the union
 //with the respective float representaion of 4 x bytes of the buffer
@@ -182,100 +225,185 @@ void send_command (void)
  Serial2.write(0x01); // send a command to the charger controller
  Serial2.flush(); // wait the TX buffer is empty, transmission is complete
  delay(1);
- digitalWrite(DE_RE, LOW); // set DE and RE to active LOW RX mode
 }
 
 // Send command to the HMI with all the buffers corresponding to
 // the float values
 void hmi_display_data (void)
 {
-float_number1 = bytes_to_float(buffer1);
+power_board_output_current_avg = bytes_to_float(power_board_output_current_avg_buff);
+Serial1.print("t0.txt=\"");
+Serial1.print(power_board_output_current_avg,1);
+Serial1.print("\"");
+Serial1.print("\xFF\xFF\xFF");
+power_board_output_voltage = bytes_to_float(power_board_output_voltage_buff);
 Serial1.print("t1.txt=\"");
-Serial1.print(float_number1,1);
+Serial1.print(power_board_output_voltage,1);
 Serial1.print("\"");
 Serial1.print("\xFF\xFF\xFF");
-float_number4 = bytes_to_float(buffer4);
+output_current_avg = bytes_to_float(output_current_avg_buff);
+Serial1.print("t2.txt=\"");
+Serial1.print(output_current_avg,1);
+Serial1.print("\"");
+Serial1.print("\xFF\xFF\xFF");
+output_voltage = bytes_to_float(output_voltage_buff);
+Serial1.print("t3.txt=\"");
+Serial1.print(output_voltage,1);
+Serial1.print("\"");
+Serial1.print("\xFF\xFF\xFF");
+generator_battery_voltage = bytes_to_float(generator_battery_voltage_buff);
+Serial1.print("t4.txt=\"");
+Serial1.print(generator_battery_voltage,1);
+Serial1.print("\"");
+Serial1.print("\xFF\xFF\xFF");
+tempearature_sensor_0 = bytes_to_float(tempearure_sensor_0_buff);
+tempearature_sensor_0 = tempearature_sensor_0/10.0;
+  if (tempearature_sensor_0 != -100.0)
+  {
+  Serial1.print("t5.txt=\""); 
+  Serial1.print(tempearature_sensor_0,1);
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  else
+  {
+  Serial1.print("t5.txt=\"");
+  Serial1.print("");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+temperature_sensor_1 = bytes_to_float(temperature_sensor_1_buff);
+temperature_sensor_1 = temperature_sensor_1/10.0;
+  if (temperature_sensor_1 != -100.0)
+  {
+  Serial1.print("t6.txt=\"");
+  Serial1.print(temperature_sensor_1,1);
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  else
+  {
+  Serial1.print("t6.txt=\"");
+  Serial1.print("");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+temperature_sensor_2 = bytes_to_float(temperature_sensor_2_buff);
+temperature_sensor_2 = temperature_sensor_2/10.0;
+  if (temperature_sensor_2 != -100.0)
+  {
+  Serial1.print("t7.txt=\"");
+  Serial1.print(temperature_sensor_2,1);
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  else
+  {
+  Serial1.print("t7.txt=\"");
+  Serial1.print("");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+temperature_sensor_3 = bytes_to_float(temperature_sensor_3_buff);
+temperature_sensor_3 = temperature_sensor_3/10.0;
+  if (temperature_sensor_3 != -100.0)
+  {
+  Serial1.print("t8.txt=\"");
+  Serial1.print(temperature_sensor_3,1);
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  else
+  {
+  Serial1.print("t8.txt=\"");
+  Serial1.print("");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+
+status = bytes_to_float(status_buff);
+  if (status == 0.0)
+  {
+  Serial1.print("t9.txt=\"");
+  Serial1.print("GENERATOR OFF");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  if (status == 1.0)
+  {
+  Serial1.print("t9.txt=\"");
+  Serial1.print("GENERATOR RUNNING");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  else if (status == 2.0)
+  {
+  Serial1.print("t9.txt=\"");
+  Serial1.print("WARMUP SYSTEM IS ACTIVE");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  else if (status == 4.0)
+  {
+  Serial1.print("t9.txt=\"");
+  Serial1.print("GENERATOR FAILED TO START");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  else if (status == 8.0)
+  {
+  Serial1.print("t9.txt=\"");
+  Serial1.print("GENERATOR PREMATURELY STOPPED");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  else if (status == 16.0)
+  {
+  Serial1.print("t9.txt=\"");
+  Serial1.print("GENERATOR FAILED TO STOP");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  else if (status == 32.0)
+  {
+  Serial1.print("t9.txt=\"");
+  Serial1.print("GENERATOR FAILED TO CHARGE");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  else if (status == 64.0)
+  {
+  Serial1.print("t9.txt=\"");
+  Serial1.print("AC INPUT FAULT");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  else if (status == 128.0)
+  {
+  Serial1.print("t9.txt=\"");
+  Serial1.print("UNDERVOLTAGE LOCKOUT CONDITION");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  else if (status == 256.0)
+  {
+  Serial1.print("t9.txt=\"");
+  Serial1.print("POWER OUTPUT FAULT");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+  else if (status == 512.0)
+  {
+  Serial1.print("t9.txt=\"");
+  Serial1.print("EMERGENCY SHUTDOWN SWITCH PRESSED");
+  Serial1.print("\"");
+  Serial1.print("\xFF\xFF\xFF");
+  }
+rssi = bytes_to_float(rssi_buff);
 Serial1.print("t10.txt=\"");
-Serial1.print(float_number4,1);
+Serial1.print(rssi,1);
 Serial1.print("\"");
 Serial1.print("\xFF\xFF\xFF");
-float_number5 = bytes_to_float(buffer5);
-Serial1.print("t13.txt=\"");
-Serial1.print(float_number5,1);
-Serial1.print("\"");
-Serial1.print("\xFF\xFF\xFF");
-float_number8 = bytes_to_float(buffer8);
-Serial1.print("t25.txt=\"");
-Serial1.print(float_number8,1);
-Serial1.print("\"");
-Serial1.print("\xFF\xFF\xFF");
-float_number9 = bytes_to_float(buffer9);
-Serial1.print("t26.txt=\"");
-Serial1.print(float_number9,1);
-Serial1.print("\"");
-Serial1.print("\xFF\xFF\xFF");
-float_number10 = bytes_to_float(buffer10);
-float_number10 = float_number10/10.0;
-  if (float_number10 != -100.0)
-  {
-  Serial1.print("t27.txt=\"");
-  Serial1.print(float_number10,1);
-  Serial1.print("\"");
-  Serial1.print("\xFF\xFF\xFF");
-  }
-  else
-  {
-  Serial1.print("t27.txt=\"");
-  Serial1.print("");
-  Serial1.print("\"");
-  Serial1.print("\xFF\xFF\xFF");
-  }
-  float_number11 = bytes_to_float(buffer11);
-  float_number11 = float_number11/10.0;
-  if (float_number11 != -100.0)
-  {
-  Serial1.print("t28.txt=\"");
-  Serial1.print(float_number11,1);
-  Serial1.print("\"");
-  Serial1.print("\xFF\xFF\xFF");
-  }
-  else
-  {
-  Serial1.print("t28.txt=\"");
-  Serial1.print("");
-  Serial1.print("\"");
-  Serial1.print("\xFF\xFF\xFF");
-  }
-  float_number12 = bytes_to_float(buffer12);
-  float_number12 = float_number12/10.0;
-  if (float_number12 != -100.0)
-  {
-  Serial1.print("t29.txt=\"");
-  Serial1.print(float_number12,1);
-  Serial1.print("\"");
-  Serial1.print("\xFF\xFF\xFF");
-  }
-  else
-  {
-  Serial1.print("t29.txt=\"");
-  Serial1.print("");
-  Serial1.print("\"");
-  Serial1.print("\xFF\xFF\xFF");
-  }
-  float_number13 = bytes_to_float(buffer13);
-  float_number13 = float_number13/10.0;
-  if (float_number13 != -100.0)
-  {
-  Serial1.print("t30.txt=\"");
-  Serial1.print(float_number13,1);
-  Serial1.print("\"");
-  Serial1.print("\xFF\xFF\xFF");
-  }
-  else
-  {
-  Serial1.print("t30.txt=\"");
-  Serial1.print("");
-  Serial1.print("\"");
-  Serial1.print("\xFF\xFF\xFF");
-  }
 }
+
+  
